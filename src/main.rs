c4cc7ea748138979ec39a3aaf09309f2238aa22e
Resavin <rairlord@gmail.com>
2023-07-07 00:39:33 +0300

    eldiro chapter 10

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+mod lexer;
+mod parser;
+mod syntax;
+use crate::parser::Parser;
+use std::io::{self, Write};
+
+fn main() -> io::Result<()> {
+    let stdin = io::stdin();
+    let mut stdout = io::stdout();
+
+    let mut input = String::new();
+
+    loop {
+        write!(stdout, "→ ")?;
+        stdout.flush()?;
+
+        stdin.read_line(&mut input)?;
+
+        let parse = Parser::new(&input).parse();
+        println!("{}", parse.debug_tree());
+
+        input.clear();
+    }
+}
